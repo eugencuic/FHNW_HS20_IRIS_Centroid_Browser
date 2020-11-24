cf292e1a62bfb195588ab3fe93702702b8618a6e
Eugen Cuic <52150846+eugencuic@users.noreply.github.com>
2020-11-24 20:54:38 +0100

    for loop images --> refactoring

--- iris/centroid_webapp/views.py
+++ iris/centroid_webapp/views.py
@@ -18,11 +18,13 @@ def index(request):
     num_centroids = 52
     num_observations = 3358
     num_timestepoberservations = 2324582
+    image_numbers = [i for i in range(1,54)]
 
     context = {
         'num_centroids': num_centroids,
         'num_observations': num_observations,
         'num_timestepoberservations' : num_timestepoberservations,
+        'images_num': image_numbers
 
     }
 
@@ -38,22 +40,3 @@ def list_view(request, img_id):
 
     return render(request, 'centroid_webapp/observation_list.html', context={'centroid' : centroid})
 
-'''
-def get_centroids(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'your-name.html', {'form': form})
-    '''
