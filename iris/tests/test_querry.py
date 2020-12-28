552c89ee88b69b52da3484dd6ac9948e84433f71
Eugen Cuic <52150846+eugencuic@users.noreply.github.com>
2020-12-28 13:25:11 +0100

    Revert "-"
    This reverts commit 252a59e7f1286e81a7abbec718d38e2d88b0c485.

--- iris/tests/test_querry.py
+++ iris/tests/test_querry.py
@@ -3,10 +3,9 @@ from centroid_webapp.models import CentroidCount
 
 
 centroid = 44
-@pytest.mark.django_db
 def test_centroid_query():
     result = CentroidCount.objects.filter(centroid=centroid).order_by('id_observation').values_list('id_observation', flat=True).distinct()
     # expecting to have no dulbicate observation showing up in template
-    assert len(result) == len(set(result))
+    assert len(result) = len(set(result))
 
 
