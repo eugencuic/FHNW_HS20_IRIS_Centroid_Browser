d6335ad5d595c477716dcfc5f5f3032e01b9173d
Eugen Cuic <52150846+eugencuic@users.noreply.github.com>
2020-12-28 13:25:15 +0100

    Revert "testing"
    This reverts commit 6279da355158e0886e09d21b3a859ad76001e9b3.

--- iris/tests/test_querry.py
+++ iris/tests/test_querry.py
@@ -6,6 +6,6 @@ centroid = 44
 def test_centroid_query():
     result = CentroidCount.objects.filter(centroid=centroid).order_by('id_observation').values_list('id_observation', flat=True).distinct()
     # expecting to have no dulbicate observation showing up in template
-    assert len(result) = len(set(result))
+    assert len(result) == len(set(result))
 
 
